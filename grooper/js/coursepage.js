771c5cbb007793f1bc04329dbf1e0195be41f2e0
hasun1226 <hasun1226@github.com>
2017-02-15 18:14:34 -0500

    Course page done

---
+++ grooper/js/coursepage.js
@@ -0,0 +1,5 @@
+$('table tbody tr  td').on('click',function(){
+    $("#GroupDescription").modal("show");
+    $("#txtfname").val($(this).closest('tr').children()[0].textContent);
+    $("#txtlname").val($(this).closest('tr').children()[1].textContent);
+});
