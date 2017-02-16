3d11e72a3403737bf5a417908b6ed457930eb75b
hasun1226 <hasun1226@github.com>
2017-02-15 20:24:22 -0500

    modal for list item should be modified

--- grooper/js/coursepage.js
+++ grooper/js/coursepage.js
@@ -1,5 +1,3 @@
 $('table tbody tr  td').on('click',function(){
     $("#GroupDescription").modal("show");
-    $("#txtfname").val($(this).closest('tr').children()[0].textContent);
-    $("#txtlname").val($(this).closest('tr').children()[1].textContent);
 });
