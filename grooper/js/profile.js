85af733adcf7e0ac04e4302ae55afd51ef7fc60f
Fredrik Wegelid <wegelid@gmail.com>
2017-02-17 15:59:22 -0500

    Add modals for editing name, email and phone number

--- grooper/js/profile.js
+++ grooper/js/profile.js
@@ -2,3 +2,9 @@ $('.search').on('click', function () {
 	window.location="coursepage.html";
 	return false;
 });
+
+$('#editModal').on('show.bs.modal', function(event) {
+  var $invoker = $(event.relatedTarget); //Button that invoked the modal
+
+  $(this).find('.modal-title').text('Edit ' + $invoker.parent().text());
+});
