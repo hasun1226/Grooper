58bb7b409da197f3ef4285a74b707f5ab3c1cbb8
hasun1226 <hasun1226@github.com>
2017-02-20 05:44:40 -0500

    To do: dashboard invitation, adding questions when create new poll, readme

--- grooper/js/coursepage.js
+++ grooper/js/coursepage.js
@@ -10,21 +10,40 @@ $('#orderModal').on('show.bs.modal', function(event) {
 		modal.find('.host').text('Host: Bill Gates')
 		modal.find('.description').html('Still thinking about the project idea')
 		
-		var grade = ['A', 'B', 'C', 'Just wanna pass'];
+		var answer = ['A', 'B', 'C', 'Just wanna pass'];
 		var form_div = $('<div class="form-group"></div>')
 		form_div.append('<div class="row"><label class="col-xs-12">Which grade do you aim for in this course?</label></div>');
 		
-		for (var i = 1; i < grade.length + 1; i++) {
-			form_div.append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadioOptions' + i + '" value="option' + i + '"/>' + grade[i-1] + '</label>');
+		for (var i = 1; i < answer.length + 1; i++) {
+			form_div.append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadioOptions' + i + '" value="option' + i + '"/>' + answer[i-1] + '</label>');
 		}
-		
 		modal.find('.questions').html(form_div).append('<a href="managepoll.html" class="btn btn-success">Edit</a>')
+		
 	} else if (getid == 2) {
 		modal.find('.modal-title').text('Social Media Application')
 		modal.find('.group-title').text('Social Media Application')
 		modal.find('.slots').text('1/4')
 		modal.find('.host').text('Host: Mark Zuckerberg')
 		modal.find('.description').html('My project idea is a social networking web application where you share your posts with your friends.<br>The key feature of the project is allowing other people to "like" your post.<br>I\'m thinking of calling this project "Facebook."<br>By completing this project, we can all use it to impress the boy/girl we like LOL')
+		
+		var form_div = $('<div class="form-group"></div>');
+		form_div.append('<label for="textarea_answer">Do you have a web application development experience?</label><textarea class="form-control" id="textarea_answer" rows="3"></textarea>');
+		
+		var form_div1 = $('<div class="form-group"></div>');
+		form_div1.append('<div class="row"><label class="col-xs-12">How high would you rate your coding skills?</label></div>');
+		for (var i = 1; i < 5 + 1; i++) {
+			form_div1.append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio' + i + '" value="option' + i + '"/>' + i + '</label>');
+		}
+		
+		var answer = ['C', 'Java', 'MySQL', 'Python', 'XML'];
+		var form_div2 = $('<div class="form-group"></div>')
+		form_div2.append('<div class="row"><label class="col-xs-12">Choose all the programming languages you can use</label></div>');
+		for (var i = 1; i < answer.length + 1; i++) {
+			form_div2.append('<label class="checkbox-inline"><input type="checkbox" id="inlineCheckbox' + i + '" value="option' + i + '"/>' + answer[i-1] + '</label>');
+		}
+		
+		modal.find('.questions').append(form_div, form_div1, form_div2, '<button type="button" class="btn btn-success">Submit</button>')
+		
 	} else {
 		modal.find('.modal-title').text('TA Application')
 		modal.find('.group-title').text('TA Application')
--- grooper/js/dashboard.js
+++ grooper/js/dashboard.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+	document.getElementById('InputName').value = 'Bill Gates'
+	document.getElementById('InputEmail').value = 'bill@microsoft.com'
+	
+	if (document.getElementById('textarea_answer')) {
+		document.getElementById('textarea_answer').defaultValue = 'bunch'
+	}
+	
+	if (document.getElementById('inlineRadio5')) {
+		document.getElementById('inlineRadio5').checked = true
+	}
+	
+	if (document.getElementById('inlineCheckbox1')) {
+		for(var i = 1; i < 5 + 1; i++) {
+			document.getElementById('inlineCheckbox' + i).checked = true
+		}
+	}
+});
+
 $('.search').on('click', function() {
 	window.location="coursepage.html";
 	return false;
@@ -20,7 +39,36 @@ $('#orderModal').on('show.bs.modal', function(event) {
 		modal.find('.slots').text('2/4')
 		modal.find('.host').text('Host: Amir Chinaei')
 		modal.find('.description').html('This project is intended to create an application that receives applications for TA positions.<br>I already have one person in the group so I just need 2 more members who are willing to learn and have some fun at the same time! No strings attached :)')
-		modal.find('.questions').html('<button type="button" class="btn btn-success">Submit</button>')
+		modal.find('.questions').html('<button type="button" class="edit btn btn-success" data-dismiss="modal">Edit</button>')
 	}
 	
+});
+
+$('.edit').on('click', function() {
+	var name = document.getElementById('InputName').value
+	document.getElementById('InputName').value = name
+	var email = document.getElementById('InputEmail').value
+	document.getElementById('InputEmail').value = email
+	if (document.getElementById('textarea_answer')) {
+		var text_ans = document.getElementById('textarea_answer').value
+		document.getElementById('textarea_answer').defaultValue = text_ans
+	}
+	
+	if (document.getElementsByName('inlineRadioOptions')) {
+		var radios = document.getElementsByName('inlineRadioOptions')
+		for (var i = 1; i < radios.length + 1; i++) {
+			if (radios[i-1].checked) {
+				document.getElementById('inlineRadio' + i).checked = true
+			}
+		}
+	}
+	
+	if (document.getElementsByName('inlineCheckbox')) {
+		var checks = document.getElementsByName('inlineCheckbox')
+		for(var i = 1; i < checks.length + 1; i++) {
+			if (checks[i-1].checked) {
+				document.getElementById('inlineCheckbox' + i).checked = true
+			}
+		}
+	}
 });
