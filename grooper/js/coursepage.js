6fee424879fa62e7b8026c84edde83f0948d6595
hasun1226 <hasun1226@github.com>
2017-02-22 00:56:24 -0500

    creating poll finished, need to do readme

--- grooper/js/coursepage.js
+++ grooper/js/coursepage.js
@@ -1,6 +1,8 @@
 var text_ans = 'bunch'
 var radio_checked = 5
 var checkbox_checked = [1,2,3,4,5]
+var questions_added
+/* Above variables are just temporary variables in order to show how the front end works */
 
 $('#orderModal').on('show.bs.modal', function(event) {
 	var row = $(event.relatedTarget) // Row that is clicked
@@ -32,7 +34,7 @@ $('#orderModal').on('show.bs.modal', function(event) {
 		for (var i = 1; i < answer.length + 1; i++) {
 			form_div.append('<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadioOptions' + i + '" value="option' + i + '"/>' + answer[i-1] + '</label>');
 		}
-		modal.find('.questions').html(form_div).append('<a href="managepoll.html" class="btn btn-success">Edit</a>')
+		modal.find('.questions').html(form_div).append('<a href="managepoll.html"><button type="button" class="btn btn-success">Edit</button></a>')
 		
 	} else if (getid == 2) {
 		modal.find('.modal-title').text('Social Media Application')
@@ -80,6 +82,18 @@ $('#orderModal').on('show.bs.modal', function(event) {
 		$.each(checkbox_checked, function(index, value) {
 			modal.find('#inlineCheckbox' + value).prop('checked', true)
 		})
+	} else if (getid > 4) {
+		modal.find('.modal-title').text("We just created a new poll")
+		modal.find('.group-title').text("We just created a new poll")
+		modal.find('.slots').text('1/4')
+		modal.find('.host').text('Host: Bill Gates')
+		modal.find('.description').html('This is to show you how a new poll is created. The page should be refreshed once a new poll is created, and the data that is added to the database will be fetched to show the contents of this modal.')
+		modal.find('.app-form').html('You made it! The questions you made are as follow.')
+		var form_div = $('<div class="form-group"></div>');
+		questions_added.each(function() {
+			form_div.append($(this))
+		})
+		modal.find('.questions').html(form_div)
 		
 	} else {
 		modal.find('.modal-title').text('TA Application')
@@ -90,7 +104,7 @@ $('#orderModal').on('show.bs.modal', function(event) {
 		modal.find('.app-form').html('You are invited!')
 		
 		// the button must be edit button if the invitation was not received and submit button if application was not submitted
-		modal.find('.questions').html('<button type="button" class="accept btn btn-success" data-dismiss="modal">Accept</button><button type="button" class="reject btn btn-danger" data-dismiss="modal">Reject</button>')
+		modal.find('.questions').html('<button type="button" class="reject btn btn-danger" data-dismiss="modal">Reject</button><button type="button" class="accept btn btn-success" data-dismiss="modal">Accept</button>')
 		$('.accept').on('click', function() {
 			if (confirm('Will you accept the invitation to this group?')) {
 				// move to 'My groups'? or leave it in the 'My applications' panel?
@@ -112,23 +126,68 @@ $('.search').on('click', function() {
 
 qNo = 1;
 
-function createQuestion() {
-	var question = document.createElement("INPUT");
-    question.id = "text" + qNo;
-    question.type = "text"; // Change
-	question.className = "form-control";
+function createQuestion() {	
+	// Generates questions
+	qNo++;
 	
-	var label = document.createElement("label");
-	label.labelfor = "sth";
-	label.innerHTML = document.getElementById('inputQuestion').value;
+	if (document.getElementById('inputQuestion').value.trim() != '') {
+		var division = document.createElement('div');
+		division.className = 'row';
+		var label = document.createElement('label');
+		label.labelfor = 'new_question';
+		label.className = 'col-xs-12';
+		label.innerHTML = document.getElementById('inputQuestion').value;
+		var remove = document.createElement('span');
+		remove.className = "remove-question btn glyphicon glyphicon-trash";
+		remove.onclick = function(){
+			remove.parentNode.parentNode.parentNode.remove();
+		};
+		label.appendChild(remove);
+		division.appendChild(label);
+		
+		var form_div = document.createElement('div');
+		form_div.className ='form-group newly_added';
+		form_div.append(division);
 	
-	var form_div = document.createElement('div');
-	form_div.className ='form-group newly_added';
+		var type = document.getElementById('question_type');
+		if (type.options[type.selectedIndex].value == 'text') {
+			var question = document.createElement('textarea');
+			question.rows = '3';
+			question.className = "form-control";
+			form_div.append(question);
+		} else if (type.options[type.selectedIndex].value == 'MC') {
+			var options = prompt("Please enter all the values for the answer separated by commas").split(",");
+			options.forEach(function createQuestion(value) {
+				if (value.trim() != '') {
+					var radio_inline = document.createElement('label');
+					radio_inline.className = 'radio-inline';
+					radio_inline.textContent = ' ' + value.trim();
+					var question = document.createElement('INPUT');
+					question.type = 'radio';
+					question.setAttribute('name', 'radio' + qNo);
+					radio_inline.prepend(question);
+					form_div.append(radio_inline);
+				}
+			});
+			
+		} else {
+			var options = prompt("Please enter all the values for the answer separated by commas").split(",");
+			options.forEach(function createQuestion(value) {
+				if (value.trim() != '') {
+					var cb_inline = document.createElement('label');
+					cb_inline.className = 'checkbox-inline';
+					cb_inline.textContent = ' ' + value.trim();
+					var question = document.createElement('INPUT');
+					question.type = 'checkbox';
+					cb_inline.prepend(question);
+					form_div.append(cb_inline);
+				}
+			});
+		}
 	
-	form_div.append(label, question);
-	
-	var app_form = document.getElementById('application_form');
-	app_form.insertBefore(form_div, app_form.lastElementChild);
+		var app_form = document.getElementById('application_form');
+		app_form.insertBefore(form_div, app_form.lastElementChild);	
+	}
 	
 	document.getElementById('inputQuestion').value = '';
 }
@@ -140,23 +199,50 @@ $('#createNewPoll').on('hidden.bs.modal', function() {
 	$('#inputCourse').val('CSC309');
 	$('#inputProjectTitle').val('');
 	$('#inputProjectDesc').val('');
+	document.getElementById('question_type').value = 'text';
+	qNo = 1;
 });
+
+var monthNames = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
 
 $('#application_form').on('submit', function(e) {
 	e.preventDefault();
-	/* Above command to stay on the same page */
+	/* Above command to stay on the same page just to see how front-end works */
 	
 	var inputCourse = $('#inputCourse').val();
+	if (inputCourse != 'CSC309') {
+		$('#createNewPoll').modal('hide');
+		// Default everything in the modal
+		$('#inputCourse').val('CSC309');
+		$('#inputProjectTitle').val('');
+		$('#inputProjectDesc').val('');
+		return;
+	}
+	
 	var inputProjectTitle = $('#inputProjectTitle').val();
 	var inputProjectDesc = $('#inputProjectDesc').val();
 
 	var select_id = document.getElementById("select_id");
 	var group_size = select_id.options[select_id.selectedIndex].value;
 	
+	// Questions that are newly added can be found with classname "newly_added"
+	$('.newly_added').find('.remove-question').remove();
+	questions_added = $('.newly_added').clone();
+	
+	var polls = $('#polls');
+	var newItem = $('<tr data-toggle="modal" data-id="' + polls.find('tr').length+1 + '" data-target="#orderModal">');
+	newItem.append('<td class="group-host col-md-3">Bill Gates</td>');
+	newItem.append('<td class="group-name col-md-6">' + inputProjectTitle + '</td>');
+	
+	var date = new Date();
+	newItem.append('<td class="col-md-3">' + monthNames[date.getMonth()] + ' ' + date.getDate() + '</td>');
+	
+	newItem.append('<td class="slots-open col-md-3">1/'+ group_size + '</td>');
+	newItem.insertBefore(polls.children(":first"));
+	
 	$('#createNewPoll').modal('hide');
 	// Default everything in the modal
 	$('#inputCourse').val('CSC309');
 	$('#inputProjectTitle').val('');
 	$('#inputProjectDesc').val('');
-	qNo = 1
 });
