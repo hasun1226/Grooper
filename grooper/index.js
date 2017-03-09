e26a12d8ea277f2212891c284e24eed515d930fa
hasun1226 <hasun1226@github.com>
2017-03-09 13:08:50 -0500

    minor changes according to index.js

--- grooper/index.js
+++ grooper/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var models = require('./model/db'); //access each model by models.Users/Polls/Applications etc
+var models = require('./model/db'); //access each model by models
 var app = express();
 
-
-//----- COPIED FROM LAB 5 - NOT SURE WHAT IT'S DOING BUT IT DIDN'T WORK WITHOUT IT -----
 // Set views path, template engine and default layout
 app.use(express.static(__dirname + '/assets'));
 app.engine('.html', require('ejs').__express);
@@ -13,7 +11,7 @@ app.set('view engine', 'html');
 
 //mongo
 var MongoClient = require('mongodb').MongoClient;
-var mongoURL = 'mongodb://localhost/grooper';
+var mongoURL = 'mongodb://localhost:3000/grooper';
 var db;
 MongoClient.connect(mongoURL, function(err, database) {
   db = database;
@@ -24,9 +22,13 @@ MongoClient.connect(mongoURL, function(err, database) {
 });
 
 app.get('/', function(req, res) {
-    res.render('register');
+    res.render('signin');
 });
 
 app.post('/signup', function(req, res) {
     res.send('do signup stuff here');
+});
+
+app.post('/login', function(req, res) {
+	res.render('dashboard');
 });
--- grooper/model/db.js
+++ grooper/model/db.js
@@ -12,21 +12,14 @@ var UserSchema = new Schema(
     phone: 
 	  { type: Number, required: true },
     pw: 
-	  { type: String, required: true }
+	  { type: String, required: true },
+	curr_course:
+	  [ code: { type: String, required: true } ],
+	prev_course:
+	  [ code: { type: String, required: true } ]
   },
   {
     collection: 'users'
-  }
-);
-
-var CourseSchema = new Schema (
-  {
-	user: { type: Number, required: true },
-	code: { type: String, required: true },
-	current: { type: Boolean, required: true }
-  },
-  { 
-    collection: 'course_history' 
   }
 );
 
@@ -39,13 +32,15 @@ var PollSchema = new Schema (
 	size: { type: Number, required: true },
 	date: { type: Date, required: true },
 	course: { type: String, required: true },
-	
-	// id helpful to have for same question but different type/options
 	question: [{ 
-	  // Need to make the fields required - DONE
-	  id: {type: Number, required: true}, q_type: {type: Number, required: true}, question: {type: String, required: true},
+	  // id helpful to have for same question but different type/options
+	  id: { type: Number, required: true }, 
+	  // 0: open, 1: single-answer MC, 2: multiple-answers MC
+	  q_type: { type: Number, required: true }, 
+	  question: { type: String, required: true },
 	  options: [{
-	    value: {type: String, required: true}, correct: Boolean
+	    value: { type: String, required: true }, 
+		correct: Boolean
 	  }]
 	}]
   },
@@ -59,9 +54,10 @@ var ApplicationSchema = new Schema (
     user: { type: Number, required: true },
 	poll: { type: Number, required: true },
 	status: { type: Number, required: true },
+	//answer is an array because it could have multiple answers
 	answers: [{
-	  question: {type: Number, required: true}, answer: [{ value: String }] //answer is an array because it could be a multiple choice question
-	  
+	  question: { type: Number, required: true }, 
+	  value: { type: String, required: true }
 	}]
   },
   {
@@ -79,17 +75,15 @@ var GroupSchema = new Schema (
   }
 );
 
-mongoose.connect('mongodb://localhost/grooper');
+mongoose.connect('mongodb://localhost:3000/grooper');
 
 //converting the schemas to models
 var Users = mongoose.model('Users', UserSchema);
-//var Courses = mongoose.model('Courses', CourseSchema);
 var Polls = mongoose.model('Polls', PollSchema);
-var Applications = mongoose.model('Courses', ApplicationSchema);
+var Applications = mongoose.model('Applications', ApplicationSchema);
 var Groups = mongoose.model('Groups', GroupSchema);
 module.exports = {
     Users: Users,
-    //Courses: Courses,
     Polls: Polls,
     Applications: Applications,
     Groups: Groups
